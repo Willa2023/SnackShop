c5d7333b4dd3e059e86ddfde484a03a1b5f23e48
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-07-04 16:24:51 +1200

    add datagrid to display snack info

--- frontend/src/Components/HomeContent.tsx
+++ frontend/src/Components/HomeContent.tsx
@@ -1,16 +1,17 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Toolbar, Typography } from '@mui/material';
 import SnackDataGrid from './SnackDataGrid';
 
 const HomeContent: React.FC = () => {
   return (
     <>
       <Box mb={2}>
+        <Toolbar></Toolbar>
         <Typography variant="h6" gutterBottom>
-          Student List
+          Snack List
         </Typography>
         <Box display="flex" gap={2} mb={2}>
           <Button variant="contained" color="primary">
-            Add Student
+            Add Snack
           </Button>
         </Box>
         <SnackDataGrid />
--- frontend/src/Components/SnackDataGrid.tsx
+++ frontend/src/Components/SnackDataGrid.tsx
@@ -1,3 +1,4 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Snack } from '../Models/Snack';
 
 interface SnackDataGridProps {
@@ -7,8 +8,69 @@ interface SnackDataGridProps {
   error: string | null;
 }
 
+const columns: GridColDef[] = [
+  { field: 'snackId', headerName: 'ID', width: 90 },
+  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'brand', headerName: 'brand', width: 150 },
+  { field: 'costPrice', headerName: 'Cost Price', width: 110 },
+  { field: 'sellPrice', headerName: 'Sell Price', width: 110 },
+  { field: 'description', headerName: 'Description', width: 230 },
+];
+
+const rows = [
+  {
+    id: 1,
+    snackId: 1,
+    name: 'Cheetos',
+    brand: 'Frito-Lay',
+    costPrice: 1.5,
+    sellPrice: 2.5,
+    description: 'Cheese Puffs',
+  },
+  {
+    id: 2,
+    snackId: 2,
+    name: 'Doritos',
+    brand: 'Matutano',
+    costPrice: 1.5,
+    sellPrice: 2.5,
+    description: 'Nacho Cheese',
+  },
+  {
+    id: 3,
+    snackId: 3,
+    name: 'Lays',
+    brand: 'PepsiCo',
+    costPrice: 1.5,
+    sellPrice: 2.5,
+    description: 'Classic',
+  },
+  {
+    id: 4,
+    snackId: 4,
+    name: 'Fritos',
+    brand: 'Lays',
+    costPrice: 1.5,
+    sellPrice: 2.5,
+    description: 'Original',
+  },
+  {
+    id: 5,
+    snackId: 5,
+    name: 'Ruffles',
+    brand: 'Sabritas',
+    costPrice: 1.5,
+    sellPrice: 2.5,
+    description: 'Sour Cream & Onion',
+  },
+];
+
 const SnackDataGrid: React.FC = () => {
-  return <div>Snack Data Grid</div>;
+  return (
+    <div style={{ height: '70vh', width: '100%' }}>
+      <DataGrid rows={rows} columns={columns}></DataGrid>
+    </div>
+  );
 };
 
 export default SnackDataGrid;
