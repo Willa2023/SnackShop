cab1275f15cd519355cb17345cfbebbec18a96f3
Willa2023 <142073064+Willa2023@users.noreply.github.com>
2024-07-20 00:14:02 +1200

    responsive for sell page datagrid

--- frontend/src/Components/Forms/SellDataGrid.tsx
+++ frontend/src/Components/Forms/SellDataGrid.tsx
@@ -1,37 +1,56 @@
-import React from 'react';
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import React, { useEffect, useRef, useState } from 'react';
+import { DataGrid, GridApi, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 import { Sell } from '../../Models/SnackStockSell';
 import SellChart from '../Charts/SellChart';
+import { Grid, useMediaQuery, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 interface SellDataGridProps {
   sells: Sell[];
 }
 
 const SellDataGrid: React.FC<SellDataGridProps> = ({ sells }) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [columnVisibilityModel, setColumnVisibilityModel] = useState({
+    id: !isSmallScreen,
+    snackName: true,
+    quantity: true,
+    date: true,
+  });
+
+  useEffect(() => {
+    setColumnVisibilityModel({
+      id: !isSmallScreen,
+      snackName: true,
+      quantity: true,
+      date: true,
+    });
+  }, [isSmallScreen]);
+
   const columns: GridColDef[] = [
     {
       field: 'id',
       headerName: 'ID',
-      width: 100,
-      editable: false,
+      flex: 0.5,
     },
     {
       field: 'snackName',
       headerName: 'Snack Name',
-      width: 200,
-      editable: false,
+      maxWidth: isSmallScreen ? 150 : 1000,
+      flex: 1,
     },
     {
       field: 'quantity',
-      headerName: 'Sell Quantity',
-      width: 150,
-      editable: false,
+      headerName: 'Quantity',
+      maxWidth: isSmallScreen ? 80 : 1000,
+      flex: 0.5,
     },
     {
       field: 'date',
-      headerName: 'Sell Date',
-      width: 150,
-      editable: false,
+      headerName: 'Date',
+      maxWidth: isSmallScreen ? 100 : 1000,
+      flex: 1,
     },
   ];
 
@@ -46,20 +65,33 @@ const SellDataGrid: React.FC<SellDataGridProps> = ({ sells }) => {
   }));
 
   return (
-    <>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: { paginationModel: { pageSize: 10, page: 0 } },
-        }}
-        pageSizeOptions={[5, 10, 20]}
-        pagination
-        paginationMode="client"
-        autoHeight
-      ></DataGrid>
-      <SellChart rows={rows} />
-    </>
+    <Grid container>
+      <Grid item xs={12} sx={{ overflowX: 'auto' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          columnVisibilityModel={columnVisibilityModel}
+          initialState={{
+            pagination: { paginationModel: { pageSize: 10, page: 0 } },
+          }}
+          pageSizeOptions={[5, 10, 20]}
+          pagination
+          paginationMode="client"
+          autoHeight
+          sx={{
+            '& .MuiDataGrid-columnHeader': {
+              typography: isSmallScreen ? '0.75rem' : '1rem',
+            },
+            '& .MuiDataGrid-cell': {
+              fontSize: isSmallScreen ? '0.75rem' : '1rem',
+            },
+          }}
+        ></DataGrid>
+      </Grid>
+      <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <SellChart rows={rows} />
+      </Grid>
+    </Grid>
   );
 };
 
